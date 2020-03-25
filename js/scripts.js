05e45bfa688649568bf89d8a26155fc36620ddbd
Markell <markell.b.richards@gmail.com>
2020-03-25 16:26:23 -0500

    welcome section cards

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,14 @@ $(function() {
   });
 });
 
-// Sticky menu background
+const smAndUp = window.matchMedia('(min-width: 578px)');
+
 window.addEventListener('scroll', function() {
   const navbar = document.getElementById('main-nav');
-  if (window.scrollY > 150) {
+  if (window.scrollY > 150 && smAndUp.matches) {
     document.querySelector('#main-nav').style.opacity = 0.9;
-    navbar.classList.add('bg-dark');
+    navbar.classList.add('bg-dark-blue');
   } else if (window.scrollY < 150) {
-    navbar.classList.remove('bg-dark');
+    navbar.classList.remove('bg-dark-blue');
   }
 });
