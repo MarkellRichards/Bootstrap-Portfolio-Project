79e683838b84e335d803b8587cd01c7133bd2521
MarkellRichards <markell.b.richards@gmail.com>
2020-03-25 23:39:52 -0500

    contact page done

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,16 @@ window.addEventListener('scroll', function() {
     navbar.classList.remove('bg-dark-blue');
   }
 });
+
+// Initialize and add the map
+function initMap() {
+  // Your location
+  const loc = { lat: 41.19097, lng: -111.96246 };
+  // Centered map on location
+  const map = new google.maps.Map(document.querySelector('.map'), {
+    zoom: 14,
+    center: loc
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({ position: loc, map: map });
+}
