4d358f1b388df91eec89357b1089fa9cb71b3a50
Markell <markell.b.richards@gmail.com>
2020-03-28 00:31:42 -0500

    applicationLogin page done

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,10 @@ $(function() {
   });
 });
 
-const smAndUp = window.matchMedia('(min-width: 578px)');
-
 window.addEventListener('scroll', function() {
   const navbar = document.getElementById('main-nav');
-  if (window.scrollY > 150 && smAndUp.matches) {
-    document.querySelector('#main-nav').style.opacity = 0.9;
+  if (window.scrollY > 150) {
+    navbar.style.opacity = 0.9;
     navbar.classList.add('bg-dark-blue');
   } else if (window.scrollY < 150) {
     navbar.classList.remove('bg-dark-blue');
